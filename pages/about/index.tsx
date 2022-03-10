b63434a0a7e0c611b19bbed9ec3d9c95455e6b76
Ahmed Sinan <asinan007@gmail.com>
2022-03-10 18:15:26 +0500

    fix about
    fix about

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,22 +1,35 @@
-import { useState } from 'react';
-import { useS3Upload } from 'next-s3-upload';
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Users from '../../components/Users'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
 
-export default function UploadTest() {
-  let [imageUrl, setImageUrl] = useState();
-  let { FileInput, openFileDialog, uploadToS3 } = useS3Upload();
-
-  let handleFileChange = async file => {
-    let { url } = await uploadToS3(file);
-    setImageUrl(url);
-  };
-
-  return (
-    <div>
-      <FileInput onChange={handleFileChange} />
-
-      <button onClick={openFileDialog}>Upload file</button>
-
-      {imageUrl && <img src={imageUrl} />}
-    </div>
-  );
+const Index: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>Lottie Assesment</title>
+                <meta name="description" content="Lottie Assesment by Sinan" />
+                <link rel="icon" href="/images/lottie.svg" />
+            </Head>
+            <Navbar />
+            <div className="w-full flex flex-col sm:flex-row flex-grow overflow-hidden">
+                <main role="main" className="w-full h-full flex-grow overflow-auto mt-5 mb-5 p-5">
+                    <h1 className='text-center text-red-600 mt-5'>About This Assesment</h1>
+                    <div className='mt-10 mb-10'>
+                        <p>
+You are tasked to create an application that will allow a user to upload an animation, view and
+search for those animations, and make minor edits
+Assume a user will use this feature to upload their own animation to see if it plays well using
+the Lottie-Player. Tag the animations with searchable fields, and browse their animations later
+to view previously uploaded animations.
+                        </p>
+                    </div>
+                </main>
+            </div>
+            <Footer />
+        </div>
+    )
 }
+
+export default Index
